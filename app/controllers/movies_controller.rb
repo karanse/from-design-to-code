45928e02da952a2d5c6ce176be01e85a769d1895
sema karan <karan18sema@gmail.com>
2024-03-05 09:51:01 +0100

    search functionality

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,14 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.order(year: :desc, title: :asc)
+
+    if params[:query].present?
+      @movies = @movies.where("title ILIKE ?", "%#{params[:query]}%")
+    end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: "movies/list", locals: { movies: @movies }, formats: [:html] }
+    end
+  end
+end
